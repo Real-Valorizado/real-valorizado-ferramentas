89c1c75fb0fcacbedb3783facbf57554b3eda120
MekkaLabs <87154227+MekkaLabs@users.noreply.github.com>
2022-05-03 11:32:54 -0300

    inverter-anterior

--- components/toolslist.tsx
+++ components/toolslist.tsx
@@ -47,28 +47,32 @@ export default function ToolsList() {
                 </div>
                 <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
                     {posts.map((post) => (
+
                         <div key={post.title} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                            <div className="flex-shrink-0">
-                                <div className="h-64 w-full relative">
-                                    <Image
-                                        src={post.imageUrl}
-                                        layout="fill"
-                                        objectFit="cover"
-                                        alt=""
-                                    />
+                            <>
+                                <div className="flex-shrink-0">
+                                    <div className="h-64 w-full relative">
+                                        <Image
+                                            src={post.imageUrl}
+                                            layout="fill"
+                                            objectFit="cover"
+                                            alt=""
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                                <div className="flex-1">
-                                    <Link href={post.href}>
-                                        <a target="_blank" className="block mt-2">
-                                            <p className="text-2xl font-bold text-blue-700">{post.title}</p>
-                                            <p className="mt-3 text-base text-gray-500">{post.description}</p>
-                                        </a>
-                                    </Link>
+                                <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                                    <div className="flex-1">
+                                        <Link href={post.href}>
+                                            <a target="_blank" className="block mt-2">
+                                                <p className="text-2xl font-bold text-blue-700">{post.title}</p>
+                                                <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                                            </a>
+                                        </Link>
+                                    </div>
                                 </div>
-                            </div>
+                            </>
                         </div>
+
                     ))}
                 </div>
             </div>
