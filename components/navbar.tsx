ea3bf24e13b381e8cfd65d6037796858da64f7bb
MekkaLabs <87154227+MekkaLabs@users.noreply.github.com>
2022-05-06 18:30:21 -0300

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
             <Head>
                 <link rel="icon" href="/favicon.png" />
             </Head>
-            <header className="bg-indigo-600">
+            <header className="bg-blue-700">
                 <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
                     <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
                         <div className="flex items-center">
